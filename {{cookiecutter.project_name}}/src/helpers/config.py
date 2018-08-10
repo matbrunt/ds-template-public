4fb4896992d4f66b4ba8249e1f7797ddf7412471
Mat Brunt <mat.brunt@skyscanner.net>
2018-08-10 09:49:32 +0100

    Updated config helper with correct paths

--- {{cookiecutter.project_name}}/src/helpers/config.py
+++ {{cookiecutter.project_name}}/src/helpers/config.py
@@ -2,10 +2,11 @@ import os
 import yaml
 from configparser import ConfigParser
 
+from helpers import utils
+
 
 def read_config():
-    config_path = os.path.abspath(os.path.join(__file__, os.pardir, 'config.yaml'))
-    config = yaml.load(open(config_path))
+    config = yaml.load(open(utils.get_project_path('config.yaml')))
     
     # Override config setting with environment variable, if it exists
     env_config = os.getenv('DEBUG', None)
@@ -16,9 +17,8 @@ def read_config():
 
 
 def read_credentials(section=None):
-    credentials_path = os.path.abspath(os.path.join(__file__, os.pardir, 'credentials.txt'))
     credentials = ConfigParser(interpolation=None)
-    credentials.read(credentials_path)
+    credentials.read(utils.get_project_path('credentials.txt'))
 
     if section is None:
         return credentials
